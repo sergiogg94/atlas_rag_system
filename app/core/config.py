1f6d3188145b1c631b3c8801e3dd73dda826cd01
sergiogg <gutierrez.galarza.s.402@gmail.com>
2026-04-11 12:31:22 -0600

    Add Settings class

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    def __init__(self):
+        # Basic settings
+        self.app_env = os.getenv("APP_ENV", "dev")
+        self.log_level = os.getenv("LOG_LEVEL", "INFO")
+
+        # Database settings
+        self.postgres_user = os.getenv("POSTGRES_USER")
+        self.postgres_password = os.getenv("POSTGRES_PASSWORD")
+        self.postgres_db = os.getenv("POSTGRES_DB")
+        self.postgres_host = os.getenv("POSTGRES_HOST", "localhost")
+        self.postgres_port = os.getenv("POSTGRES_PORT", "5432")
+
+
+settings = Settings()
